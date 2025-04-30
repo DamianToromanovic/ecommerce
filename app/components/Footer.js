8f3990f66b472672b26ec2ac32a1e8a1fdc88e78
Luca224lu90 <ammirati1990@gmail.com>
2025-04-30 16:08:51 +0200

    footer ein wenig bearbeitet

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -77,6 +77,7 @@ export default function Footer() {
           <Link href="/kontakt" className="hover:underline">
             Kontakt
           </Link>
+          <a href="/aboutUs/page.js">über uns</a>
         </div>
         <p className="text-sm text-gray-600 mt-6">
           &copy; 2025 BadezimmerWelt. Alle Rechte vorbehalten.
