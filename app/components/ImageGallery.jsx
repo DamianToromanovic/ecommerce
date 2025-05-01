76cd57f1031cefaa92c9626e011fecf98ca71289
Khushbu551 <khushbu.patel@dci-student.org>
2025-05-01 19:41:19 +0200

    tabs fertig

---
+++ app/components/ImageGallery.jsx
@@ -0,0 +1,36 @@
+"use client";
+import { useState, useEffect } from "react";
+
+export default function ImageGallery({ images = [] }) {
+  const [selectedImage, setSelectedImage] = useState(images[0]);
+
+  useEffect(() => {
+    setSelectedImage(images[0]);
+  }, [images]);
+
+  return (
+    <div className="flex gap-4">
+      {/* Left side: 3 thumbnaills */}
+      <div className="flex flex-col gap-2">
+        {images.slice(0, 5).map((img, index) => (
+          <img
+            key={index}
+            src={img}
+            alt={`Thumbnail ${index + 1}`}
+            onClick={() => setSelectedImage(img)}
+            className="w-full h-20 object-cover rounded-md mb-2"
+          ></img>
+        ))}
+      </div>
+
+      {/* right side mmain image */}
+      <div>
+        <img
+          src={selectedImage}
+          alt="Selected"
+          className="w-[500px] h-[400px] object-cover rounded shadow"
+        ></img>
+      </div>
+    </div>
+  );
+}
