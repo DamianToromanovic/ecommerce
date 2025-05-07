91fafb52d14ef6402a740c533df2e63662dd9079
Khushbu551 <khushbu.patel@dci-student.org>
2025-05-07 13:18:01 +0200

    form

--- app/cart/page.js
+++ app/cart/page.js
@@ -16,7 +16,7 @@ export default function CartPage() {
     setTimeout(() => {
       if (cart.length === 0)
         return (
-          <div className="p-8">
+          <div className="p-8 text-center">
             <h1 className="text-2xl font-bold mb-4">
               Dein Warenkorb ist leer 🛒
             </h1>
@@ -30,7 +30,9 @@ export default function CartPage() {
 
   return (
     <div className="p-8 max-w-3xl mx-auto">
-      <h1 className="text-2xl font-bold mb-6">Dein Warenkorb</h1>
+      <h1 className="text-2xl font-bold mb-8 text-center text-blue-800 font-serif">
+        Dein Warenkorb
+      </h1>
 
       {cart.map((item) => (
         <div key={item.id} className=" py-4 flex justify-between">
@@ -42,20 +44,20 @@ export default function CartPage() {
           <div className="flex items-center gap-2">
             <button
               onClick={() => decreaseQuantity(item.id)}
-              className="px-2 py-1 bg-gray-200 rounded"
+              className="px-3 py-1 bg-gray-200 rounded hover:bg-gray-300 cursor-pointer"
             >
               -
             </button>
             <span>{item.quantity}</span>
             <button
               onClick={() => addToCart({ ...item, quantity: 1 })}
-              className="px-2 py-1 bg-gray-200 rounded"
+              className="px-3 py-1 bg-gray-200 rounded hover:bg-gray-300 cursor-pointer"
             >
               +
             </button>
             <button
               onClick={() => removeFromCart(item.id)}
-              className="text-red-600 text-sm underline ml-2"
+              className="text-white bg-red-600 hover:bg-red-700 px-3 py-1 rounded text-sm cursor-pointer "
             >
               Entfernen
             </button>
@@ -63,20 +65,20 @@ export default function CartPage() {
         </div>
       ))}
 
-      <div className="flex justify-between items-center mt-6 font-semibold">
+      <div className="flex justify-between items-center mt-8 border-t pt-4 text-lg font-semibold">
         <span>Gesamt:</span>
         <span>{getTotalPrice().toFixed(2)} €</span>
       </div>
 
-      <div className="mt-6 flex justify-between">
+      <div className="mt-8 flex justify-between">
         <button
           onClick={clearCart}
-          className="bg-gray-200 px-4 py-2 rounded hover:bg-gray-300"
+          className="bg-gray-200 px-5 py-2 rounded hover:bg-gray-500 cursor-pointer"
         >
           Warenkorb leeren
         </button>
         <Link href="/checkout">
-          <button className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800">
+          <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-800 cursor-pointer">
             Zur Kasse
           </button>
         </Link>
--- app/checkout/page.js
+++ app/checkout/page.js
@@ -5,13 +5,46 @@ import CheckoutSummary from "../components/CheckoutSummary.js";
 
 export default function CheckoutPage() {
   return (
-    <div className="max-w-4xl mx-auto py-12 px-4">
-      <Link href={"/thank-you"}>
-        <button className="text-3xl font-bold mb-6">Zur Kasse</button>
-      </Link>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <CheckoutForm />
-        <CheckoutSummary />
+    <div className="min-h-screen bg-gradient-to-br from-blue-100 to-blue-200 px-4 py-16 flex flex-col items-center">
+      {/* Gutscheincode section */}
+      <div className="w-full max-w-4xl  rounded-xl shadow-lg p-6 mb-6 text-center">
+        <h2 className="text-3xl font-bold text-blue-700 mb-2">
+          Gutscheincode einlösen
+        </h2>
+        <p className="text-gray-600 text-sm mb-4">
+          Ihre Rabatt wartet auf Sie! Geben Sie unter Ihren Gutscheincode ein.
+        </p>
+        <div className="flex justify-center gap-2">
+          <input
+            type="text"
+            placeholder="Code eingeben"
+            className="border border-gray-300 px-4 py-2 rounded w-64"
+          ></input>
+          <button className="bg-blue-700 hover:bg-blue-800 text-white px-4 py-2 rounded cursor-pointer">
+            Einlösen
+          </button>
+        </div>
+      </div>
+
+      {/* Checkout section */}
+      <div className="grid grid-cols-1 md:grid-cols-2 max-w-5xl w-full bg-white rounded-xl shadow-lg overflow-hidden">
+        {/* left side */}
+        <div className="bg-white p-8">
+          <h2 className="text-2xl font-semibold mb-6 text-blue-700 font-serif text-center">
+            Lieferdetails
+          </h2>
+          <CheckoutForm />
+        </div>
+
+        {/* Right side summary */}
+        <div className="bg-blue-500 p-8 text-white">
+          <CheckoutSummary />
+          <Link href={"/thank-you"}>
+            <button className="mt-30 w-full bg-white text-blue-700 hover:bg-blue-100 font-bold py-2 px-4 rounded cursor-pointer">
+              Zur Kasse
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
