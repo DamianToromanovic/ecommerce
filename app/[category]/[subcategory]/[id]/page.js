88d1de42ca50110c43e29c7ff63e72471dcdc598
DamianToromanovic <d.toromanovic@web.de>
2025-05-08 09:46:05 +0200

    d

--- app/[category]/[subcategory]/[id]/page.js
+++ app/[category]/[subcategory]/[id]/page.js
@@ -1,113 +1,106 @@
 "use client";
 import { notFound } from "next/navigation";
 import products from "../../../lib/flatProducts.js";
-
 import ImageGallery from "../../../components/ImageGallery.js";
 import { useCartStore } from "@/app/store/cartStore.js";
 import ProductTabs from "../../../components/ProductTabs.js";
 import { useState } from "react";
-import { use } from "react";
-
 export default function ProductDetailPage({ params }) {
-  const { category, subcategory, id } = use(params);
+  const { category, subcategory, id } = params;
   const { addToCart, increaseQuantity, decreaseQuantity } = useCartStore();
   const [added, setAdded] = useState();
   const [quantity, setQuantity] = useState(1);
-
   const handleAdd = () => {
     addToCart({ ...product, quantity });
     setAdded(true);
     setTimeout(() => setAdded(false), 450);
   };
-
   const product = products.find(
     (p) =>
       p.id === id && p.category === category && p.subcategory === subcategory
   );
-
   if (!product) {
     notFound();
   }
-
   return (
     <>
-      <div className="w-1/2 mx-auto bg-orange-100 border border-orange-300 text-center  py-2 mb-4 rounded-sm mt-4">
+      <div className="w-full mx-auto bg-orange-100 border border-orange-300 text-center  py-2 mb-4 rounded-sm mt-1">
         <span className="text-sm text-orange-700 font-medium">
           Sparen Sie jetzt 5% mit dem Code:
           <span className="font-bold">hsk5</span>
         </span>
       </div>
-
-      <main className="w-full lg:flex lg:flex-col lg:justify-center px-8 mx-auto">
-        <div className="flex flex-col lg:flex-row lg:justify-center gap-8">
-          <div className="w-full max-w-screen-lg mx-auto flex flex-col lg:flex-row gap-8 px-4 mt-6">
-            <ImageGallery images={product.images} />
+      {/* Main layout */}
+      <div className="flex flex-col lg:flex-row gap-8 px-4 md:px-8 max-w-6xl mx-auto">
+        {/* Images Section */}
+        <div className="w-full lg:w-[50%]">
+          <ImageGallery images={product.images} />
+        </div>
+        {/* Product info */}
+        <div className="w-full lg:w-[50%] rounded-lg p-6 space-y-5">
+          <h1 className="text-3xl text-center font-bold text-blue-700 font-serif ">
+            {product.name}
+          </h1>
+          <p className="text-gray-600 text-sm  mb-1 ">{product.description}</p>
+          <div className="flex justify-center">
+            <div className="text-center ">
+              {product.oldPrice && (
+                <p className="text-sm text-gray-400 line-through mt-2">
+                  {product.oldPrice} €
+                </p>
+              )}
+              <p className="text-2xl text-blue-600 font-bold mt-2 ">
+                {product.price} €
+              </p>
+              <p className="text-gray-600 text-sm mb-2 font-semibold">
+                Alle Preise inkl. 19% MwSt.
+              </p>
+              <p className="text-gray-600 text-sm mt-4 font-semibold ">
+                Versandkostenfrei innerhalb Deutschlands
+              </p>
+            </div>
           </div>
-
-          <div className="w-full  rounded-lg p-6 space-y-5">
-            <h2 className="text-3xl font-bold text-blue-700 ">
-              {product.name}
-            </h2>
-
-            <p className="text-gray-600 text-sm  mb-1 ">
-              {product.description}
-            </p>
-
-            <p className="text-sm text-gray-400 line-through mb-2">
-              {product.oldPrice ? `${product.oldPrice} €` : ""}{" "}
-            </p>
-
-            <p className="text-2xl text-blue-600  font-bold mb-6">
-              {product.price} €
-            </p>
-
-            <p className="text-gray-600 text-sm  mb-3 ">
-              alle Preise inkl. 19% MwSt.
-            </p>
-
-            <p className="text-gray-600 text-sm  mb-2 font-semibold ">
-              Versandkostenfrei innerhalb Deutschlands
-            </p>
-
-            <div className="flex items-center gap-2 mb-4">
-              <button
-                onClick={() => setQuantity((q) => Math.max(1, q - 1))}
-                className="px-2 py-1 border cursor-pointer"
-              >
-                -
-              </button>
-              <span>{quantity}</span>
-              <button
-                onClick={() => setQuantity((q) => q + 1)}
-                className="px-2 py-1 border cursor-pointer"
-              >
-                +
-              </button>
-            </div>
-            <div className="flex flex-col gap-4 max-w-3xs">
-              <button
-                onClick={handleAdd}
-                className={`px-4 py-2 rounded w-full transition cursor-pointer 
-    ${added ? "bg-green-600" : "bg-blue-700 hover:bg-blue-800"} 
+          {/* Quantity sector */}
+          <div className="flex justify-center items-center gap-2 mt-4">
+            <button
+              onClick={() => setQuantity((q) => Math.max(1, q - 1))}
+              className="px-2 py-1 border cursor-pointer"
+            >
+              -
+            </button>
+            <span>{quantity}</span>
+            <button
+              onClick={() => setQuantity((q) => q + 1)}
+              className="px-2 py-1 border cursor-pointer"
+            >
+              +
+            </button>
+          </div>
+          {/* Add to cart */}
+          <button
+            onClick={handleAdd}
+            className={`px-4 py-2 rounded w-full transition cursor-pointer
+    ${added ? "bg-green-600" : "bg-blue-700 hover:bg-blue-800"}
     text-white`}
-              >
-                {added ? "✔ Hinzugefügt" : "In den Warenkorb"}
-              </button>
-
-              <div className="grid grid-cols-2 gap-2">
-                <button className="flex items-center justify-center gap-2  py-2 border rounded-md text-gray-700 bg-gray-300 shadow-sm hover:bg-gray-100 transition cursor-pointer">
-                  <span>Artikel merken</span>
-                </button>
-
-                <button className="flex items-center justify-center gap-2   py-2 border rounded-md text-gray-700 shadow-sm hover:bg-gray-100 bg-gray-300 transition cursor-pointer">
-                  <span>Fragen zum Produkt</span>
-                </button>
-              </div>
-            </div>
+          >
+            {added ? ":starkes_häkchen: Hinzugefügt" : "In den Warenkorb"}
+          </button>
+          {/* Action BUtton */}
+          <div className="grid grid-cols-2 gap-2 mt-4">
+            <button className="flex items-center justify-center gap-2  py-2 border rounded-md text-gray-700 bg-gray-300 shadow-sm hover:bg-gray-200 transition cursor-pointer">
+              <span>Artikel merken</span>
+            </button>
+            <button className="flex items-center justify-center gap-2  py-2 border rounded-md text-gray-700 bg-gray-300 shadow-sm hover:bg-gray-200 transition cursor-pointer">
+              <span>Fragen zum Produkt</span>
+            </button>
           </div>
         </div>
+      </div>
+      {/* Tab section */}
+      <div className="mt-10 px-4">
         <ProductTabs />
-      </main>
+      </div>
+      <div>hh</div>
     </>
   );
 }
--- app/components/ImageGallery.js
+++ app/components/ImageGallery.js
@@ -1,47 +1,29 @@
 "use client";
 import { useState, useEffect } from "react";
-import Image from "next/image";
-
 export default function ImageGallery({ images = [] }) {
   const [selectedImage, setSelectedImage] = useState(images[0]);
-
   useEffect(() => {
     setSelectedImage(images[0]);
   }, [images]);
-
   return (
-    <div className="flex flex-col gap-6 ">
-      <div className="flex justify-center items-center w-full ">
-        <div className="w-full max-w-[800px] relative aspect-[4/3]">
-          <Image
-            src={selectedImage}
-            alt={`Produktbild Ansicht`}
-            fill
-            className="object-cover rounded shadow"
-            sizes="(max-width: 800px) 100vw, 800px"
-            priority
-          />
-        </div>
-      </div>
-
-      <div className="flex gap-2 overflow-x-auto">
+    <div className="flex flex-col md:flex-row gap-4">
+      <div className="flex  md:flex-col gap-2 justify-center md:justify-start">
         {images.slice(0, 5).map((img, index) => (
-          <button
+          <img
             key={index}
-            className="relative w-20 h-20 cursor-pointer rounded-md border-2 overflow-hidden"
+            src={img}
+            alt={`Thumbnail ${index + 1}`}
             onClick={() => setSelectedImage(img)}
-          >
-            <Image
-              src={img}
-              alt={`Thumbnail ${index + 1}`}
-              fill
-              className={`object-cover ${
-                selectedImage === img ? "border-blue-600" : "border-transparent"
-              }`}
-              sizes="80px"
-            />
-          </button>
+            className="w-full h-25 object-cover rounded-md border hover:ring-2 ring-blue-500 cursor-pointer "
+          ></img>
         ))}
+      </div>
+      <div>
+        <img
+          src={selectedImage}
+          alt="Selected"
+          className=" w-full max:w-[500px] h-[420px] md:h-[480px ] object-cover rounded shadow"
+        ></img>
       </div>
     </div>
   );
